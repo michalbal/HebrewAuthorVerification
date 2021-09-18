d66b8736c979cf81712b284d95336bbb7e391cbd
Michal Balaban <michal.balaban@cs.huji.ac.il>
2021-09-18 22:14:44 +0300

    Removed some unecessary files

--- yap_caller.py
+++ yap_caller.py
@@ -1,12 +1,7 @@
-import pandas as pd
-import stanza
+
 import requests
-import json
 from time import sleep
-from pandas.io.json import json_normalize
-from enums import *
-from hebtokenizer import HebTokenizer
-import yap_api
+
 
 yap_token = "4c50c422eeaeeae2decdcb28f6548c7f"
 
@@ -35,17 +30,12 @@ def parse_dep_tree(dep_tree):
 
 
 def segment_and_tag_sentence(text):
-    # text = text.replace(r'"', r'\"')
     json = call_yap(text)
-    # print("json is:\n", json)
     tokenized_text = json['tokenized_text']
     segmented_text = json['segmented_text']
     lemmas = json['lemmas']
     dep_tree = json['dep_tree']
-    # print("tokenized_text: \n", tokenized_text, "\n segmented_text:\n",
-    #       segmented_text, "\nlemmas:\n", lemmas)
     words, pos_tags, suf_and_gen_info = parse_dep_tree(dep_tree)
-    # print("words are: ", words, " \npos tags are ", pos_tags)
     return tokenized_text, words, pos_tags, suf_and_gen_info
 
 
